1728084739592feaee631af62a71bb392bdb65ea
Benjamin Boruff <ben@boruff.me>
2018-10-09 15:33:33 -0400

    home page added

---
+++ resources/views/home.blade.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>SPMAPI</title>
+
+    <link rel="stylesheet" href="css/app.css">
+    <link rel="stylesheet" href="css/app2.css">
+
+</head>
+
+<body>
+    <div class="container">
+        <header class="header">
+            <nav class="nav">
+                <h1>SPMAPI</h1>
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="https://github.com/localgyros/spmapi">SPMAPI on Github</a>
+                    </li>
+                </ul>
+            </nav>
+
+        </header>
+        <main>
+            <div class="jumbotron">
+                <h2>Coming Soon ...</h2>
+                <p class="lead">Space Missions API</p>
+            </div>
+        </main>
+    </div>
+    <script src="js/app.js"></script>
+</body>
+
+</html>
